181fae7cff864f26bb6ee2a5f94a08e83dc977ef
Gerardo Barcia Palacios <gerardobarciap@gmail.com>
2012-11-04 14:49:21 +0100

    adding intial structure for web app

---
+++ web/expert_catalogue/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExpertCatalogue::Application.config.secret_token = '978870d7500eb8439a27e6e9c88fe6ee5b35c371f181a3da1c4169145347ba868ca8d501f9fdf224be3a7d450169eaf736372a442d51860d64178fb2e7ff41f0'
